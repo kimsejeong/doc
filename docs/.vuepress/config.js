8c296bf146b53e642e4db35d5285a0b2857be339
unknown <zhangtianqi@yihce.com>
2021-10-28 17:00:27 +0800

    提交

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -32,21 +32,22 @@ module.exports = {
     ],
 
     //侧边导航栏：会根据当前的文件路径是否匹配侧边栏数据，自动显示/隐藏
-    sidebar: {
-      "/pages/vue/": [
-        {
-          title: "原理", // 一级菜单名称
-          collapsable: true, // false为默认展开菜单, 默认值true是折叠,
-          sidebarDepth: 1, //  设置侧边导航自动提取markdown文件标题的层级，默认1为h2层级
-          children: [
-            ["diff.md", "diff"], //菜单名称为'子菜单1'，跳转至/pages/folder1/test1.md
-            ["react.md", "react"],
-          ],
-        },
-      ],
+    // sidebar: {
+    //   "/pages/vue/": [
+    //     {
+    //       title: "原理", // 一级菜单名称
+    //       collapsable: true, // false为默认展开菜单, 默认值true是折叠,
+    //       sidebarDepth: 1, //  设置侧边导航自动提取markdown文件标题的层级，默认1为h2层级
+    //       children: [
+    //         ["diff.md", "diff"], //菜单名称为'子菜单1'，跳转至/pages/folder1/test1.md
+    //         ["react.md", "react"],
+    //       ],
+    //     },
+    //   ],
 
-      //...可添加多个不同的侧边栏，不同页面会根据路径显示不同的侧边栏
-    },
+    //   //...可添加多个不同的侧边栏，不同页面会根据路径显示不同的侧边栏
+    // },
   },
   // base:'.'
+  "vuepress-plugin-auto-sidebar": {},
 };
